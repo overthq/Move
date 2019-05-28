c630cc1fdba4475d53c86e7982f163044d4af8c7
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-05-28 12:26:50 +0100

    Navigate to Main page automatically

--- packages/client/src/screens/Onboarding/index.tsx
+++ packages/client/src/screens/Onboarding/index.tsx
@@ -9,7 +9,9 @@ import {
 	View,
 	StyleSheet
 } from 'react-native';
+import { connect } from 'react-redux';
 import { NavigationScreenProps } from 'react-navigation';
+import { AppState } from '@move/core';
 
 import Slide from './Slide';
 import Pagination from './Pagination';
@@ -17,7 +19,17 @@ import slides from './slides';
 
 const { width } = Dimensions.get('window');
 
-const Onboarding = (props: NavigationScreenProps) => {
+interface OnboardingProps extends NavigationScreenProps {
+	accessToken: string;
+}
+
+const Onboarding = (props: OnboardingProps) => {
+	React.useEffect(() => {
+		if (!!props.accessToken) {
+			props.navigation.navigate('Main');
+		}
+	}, []);
+
 	const scrollX = new Animated.Value(0);
 
 	const skip = () => {
@@ -84,4 +96,8 @@ const styles = StyleSheet.create({
 	}
 });
 
-export default Onboarding;
+const mapStateToProps = ({ auth }: AppState) => ({
+	accessToken: auth.accessToken
+});
+
+export default connect(mapStateToProps)(Onboarding);
