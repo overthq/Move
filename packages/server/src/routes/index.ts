5ccda58ba4852007801982e68f6735a716519848
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-07-15 00:23:16 +0100

    Connect routes to main router

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
 import auth from './auth';
-import tickets from './tickets';
 import creditCards from './credit-cards';
+import rides from './rides';
+import trips from './trips';
 
 const router = Router();
 
 router.use('/auth', auth);
-router.use('/tickets', tickets);
 router.use('/credit-cards', creditCards);
+router.use('/rides', rides);
+router.use('/trips', trips);
 
 export default router;
