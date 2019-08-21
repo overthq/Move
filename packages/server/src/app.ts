a3bbf4594b10cb79cfd7d98918da7dd314b30231
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-08-21 22:22:25 +0100

    Fix graphiql issue

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import schema from './schema';
 
 const app = express();
 
-app.use('/', graphqlHTTP({ qraphiql: true }));
+app.use('/', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(4000, () => `Server started at port 4000`);
