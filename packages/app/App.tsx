6bcc4d38875ea7ecd45769603fbcadbcab40f006
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-08-24 01:29:42 +0100

    Move files into appropriate folders

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Provider, createClient } from 'urql';
 import * as Constants from 'expo-constants';
-import RootNavigator from './src/navigation';
+import RootNavigator from './src/Root';
 
 const devApiUrl = Constants.default.manifest.debuggerHost
 	.split(':')
 	.shift()
 	.concat(':4000/');
 
-const client = createClient({
-	url: devApiUrl
-});
+const client = createClient({ url: devApiUrl });
 
 const App = () => (
 	<Provider value={client}>
--- packages/app/src/screens/Auth/Login.tsx
+++ packages/app/src/screens/Auth/Login.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { View, TextInput } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+import { useMutation } from 'urql';
+import { AUTH_LOGIN } from '@move/core';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Login = () => {
 	const [phoneNumber, setPhoneNumber] = React.useState('');
-
+	const [res, executeMutation] = useMutation(AUTH_LOGIN);
 	const handleTextChange = (text: string) => {
 		setPhoneNumber(text);
+	};
+
+	const handleSubmit = async () => {
+		await executeMutation({ phoneNumber });
+		console.log(res);
 	};
 
 	return (
 		<View style={{ flex: 1 }}>
 			<TextInput onChangeText={handleTextChange} />
+			<TouchableOpacity onPress={handleSubmit}>
+				<Text>Next</Text>
+			</TouchableOpacity>
 		</View>
 	);
 };
