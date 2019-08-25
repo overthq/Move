df4f92a85f3a3831e4800c78da4ee08ba3a1fb60
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-08-25 21:43:08 +0100

    Switch VerifyCode component to mutation

--- packages/app/src/screens/Auth/VerifyCode.tsx
+++ packages/app/src/screens/Auth/VerifyCode.tsx
@@ -5,21 +5,21 @@ import {
 	TouchableOpacity,
 	KeyboardAvoidingView
 } from 'react-native';
-import { Context, createRequest } from 'urql';
+import { useMutation } from 'urql';
 import { NavigationScreenProps } from 'react-navigation';
 import { AUTH_VERIFY_CODE } from '@move/core';
 import styles from './styles';
 
 const VerifyCode = ({ navigation }: NavigationScreenProps) => {
 	const [code, setCode] = React.useState('');
-	const client = React.useContext(Context);
-
+	const [res, executeMutation] = useMutation(AUTH_VERIFY_CODE);
 	const handleTextChange = (text: string) => setCode(text);
 
 	const handleSubmit = async () => {
 		const phoneNumber: string = navigation.getParam('phoneNumber', '');
-		const request = createRequest(AUTH_VERIFY_CODE, { phoneNumber, code });
-		await client.executeQuery(request);
+		await executeMutation({ phoneNumber, code });
+		console.log(res); // And save the auth data in AsyncStorage.
+		navigation.navigate('Home');
 	};
 
 	return (
