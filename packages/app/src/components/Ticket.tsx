b991131d9d73d4ea45763c0a9db48694e066b085
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-09-06 13:14:11 +0100

    More Ticket styling

--- packages/app/src/components/Ticket.tsx
+++ packages/app/src/components/Ticket.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Ticket as TicketProps } from '@move/types';
 
-const Ticket = ({ route }: TicketProps) => (
+const Ticket = ({ route, quantity }: TicketProps) => (
 	<View style={styles.container}>
-		<Text>{route.origin.name}</Text>
-		<Text>{route.destination.name}</Text>
+		<Text>
+			{route.origin.name} to {route.destination.name}
+		</Text>
+		<View>
+			<Text>Fare: {route.fare} naira</Text>
+			<Text>{quantity}</Text>
+		</View>
 	</View>
 );
 
 const styles = StyleSheet.create({
 	container: {
+		borderWidth: 1,
+		borderColor: '#161616',
 		borderRadius: 6
 	}
 });
