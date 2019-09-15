3295cd39c1ec9d8db976c7e09afb235383684b6f
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-09-15 22:26:54 +0100

    Make Settings close button work

--- packages/app/src/screens/Settings/index.tsx
+++ packages/app/src/screens/Settings/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Feather } from '@expo/vector-icons';
+import { TouchableWithoutFeedback } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Feather } from '@expo/vector-icons';
 import SettingsHome from './Settings';
 import CreditCards from './CreditCards';
-import { StatusBar } from 'react-native';
-
-const HeaderClose = ({ onPress }: { onPress(): void }) => (
-	<Feather name='x' size={18} color='#D3D3D3' {...{ onPress }} />
-);
 
 const Settings = createStackNavigator(
 	{
@@ -15,12 +11,10 @@ const Settings = createStackNavigator(
 			screen: SettingsHome,
 			navigationOptions: ({ navigation }) => ({
 				title: 'Settings',
-				// eslint-disable-next-line
-				headerLeft: () => (
-					<>
-						<StatusBar barStyle='light-content' animated />
-						<HeaderClose onPress={() => navigation.goBack()} />
-					</>
+				headerLeft: (
+					<TouchableWithoutFeedback onPress={() => navigation.navigate('Home')}>
+						<Feather name='x' size={20} color='#D3D3D3' />
+					</TouchableWithoutFeedback>
 				),
 				headerLeftContainerStyle: {
 					paddingHorizontal: 10
