494d3bc792869f8f347acc430bd17cd3406aac61
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-10-18 03:32:36 +0100

    Remove the loaders helpers

--- packages/server/src/busStops/query.ts
+++ packages/server/src/busStops/query.ts
@@ -1,24 +1,27 @@
-import { BusStop } from '../models';
-import { routesLoader, busStopsLoader } from '../helpers/loaders';
+import { BusStop, Route } from '../models';
 
 const busStopQuery = {
 	busStops: async () => {
-		const allBusStops = await BusStop.find().populate('routes');
+		const allBusStops = await BusStop.find();
+		// DISCLAIMER: This is a very expensive operation.
+		// This should be something that should be done on the database end without duplicating data.
+		// Option 1: Store references to the routes in an array on the busStop object.
 		const allBusStopsWithRoutes = await Promise.all(
 			allBusStops.map(async busStop => {
-				const routeConditions = [
-					{ origin: busStop.id },
-					{ destination: busStop.id }
-				];
-				const routes = await routesLoader.load(routeConditions);
+				const routes = await Route.find({
+					$or: [{ origin: busStop.id }, { destination: busStop.id }]
+				}).populate('origin destination');
 				return Object.assign(busStop, { routes });
 			})
 		);
 		return allBusStopsWithRoutes;
 	},
 	busStop: async (_, { id }) => {
-		const matchedBusStop = await busStopsLoader.load(id);
-		return matchedBusStop;
+		const matchedBusStop = await BusStop.findById(id);
+		const routes = await Route.find({
+			$or: [{ origin: matchedBusStop.id }, { destination: matchedBusStop.id }]
+		}).populate('origin destination');
+		return Object.assign(matchedBusStop, { routes });
 	}
 };
 
