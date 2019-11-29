4dd102b294a3c5c10ae27030da8cd3aeddd02efb
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-11-29 21:55:53 +0100

    Add env files in preparation for dashboard deploy

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider, createClient } from 'urql';
 import AppRouter from './pages';
 
-const client = createClient({ url: 'http://localhost:4000' });
+const client = createClient({ url: process.env.REACT_APP_API_URL as string });
 
 const App: React.FC = () => (
 	<Provider value={client}>
