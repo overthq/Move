6e5ebd7efafdfa502082780ae507ca267930d0b1
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-12-09 05:13:23 +0100

    Begin work on Stops page development

--- packages/dashboard/src/pages/Stops.tsx
+++ packages/dashboard/src/pages/Stops.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import Modal from 'react-modal';
+import { Dialog } from '@reach/dialog';
 import { useBusStopsQuery } from '@move/core';
 import CreateStop from './CreateStop';
+import '@reach/dialog/styles.css';
 
 interface CreateBusStopProps {
 	isOpen: boolean;
-	openModal(): void;
+	open(): void;
+	close(): void;
 }
 
-const CreateBusStop = ({ isOpen, openModal }: CreateBusStopProps) => (
+const CreateBusStop = ({ isOpen, open, close }: CreateBusStopProps) => (
 	<div>
-		<button onClick={openModal}>Create</button>
-		<Modal isOpen={isOpen}>
+		<button onClick={open}>Add new</button>
+		<Dialog aria-label='Add new bus stop' isOpen={isOpen} onDismiss={close}>
 			<h2>Create Bus Stop</h2>
 			<CreateStop />
-		</Modal>
+		</Dialog>
 	</div>
 );
+
+const StopDetailWrapper = styled.div`
+	background-color: #ffffff;
+	border-radius: 4px;
+	padding: 10px;
+	margin-bottom: 10px;
+
+	h3 {
+		margin-top: 0;
+	}
+`;
 
 const Stops: React.FC = () => {
 	const [{ fetching, data, error }] = useBusStopsQuery();
@@ -27,21 +41,23 @@ const Stops: React.FC = () => {
 	if (error) return <p>Error</p>;
 	if (!data) return null;
 
-	const openModal = () => setModalOpen(true);
+	const open = () => setModalOpen(true);
+	const close = () => setModalOpen(false);
 
 	return (
 		<div>
 			<div style={{ display: 'flex', alignItems: 'center' }}>
-				<h1 style={{ margin: '0 10px 0 0' }}>Bus Stops</h1>
-				<CreateBusStop isOpen={modalOpen} {...{ openModal }} />
+				<h1 style={{ marginRight: 10 }}>Bus Stops</h1>
+				<CreateBusStop isOpen={modalOpen} {...{ open, close }} />
 			</div>
-			<ul>
-				{data.busStops.map(busStop => (
-					<li key={busStop._id}>
-						<Link to={`/stops/${busStop._id}`}>{busStop.name}</Link>
-					</li>
-				))}
-			</ul>
+			{data.busStops.map(({ _id, name, routes }) => (
+				<StopDetailWrapper key={_id}>
+					<Link to={`/stops/${_id}`}>
+						<h3>{name}</h3>
+					</Link>
+					<span>{routes.length} routes</span>
+				</StopDetailWrapper>
+			))}
 		</div>
 	);
 };
