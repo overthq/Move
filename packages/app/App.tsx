c4f3e7e7cac3581de21ca3277196a15cbbe47373
Oluwakorede Fashokun <koredefashokun@gmail.com>
2020-06-20 23:45:42 +0100

    Remove urql storage adapter

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
-import {
-	cacheExchange,
-	StorageAdapter,
-	SerializedEntries
-} from '@urql/exchange-graphcache';
+import { Provider, createClient } from 'urql';
 import { enableScreens } from 'react-native-screens';
 import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import RootNavigator from './src/Root';
@@ -14,26 +9,7 @@ import env from './env';
 
 enableScreens();
 
-// I have to find a storageAdapter written for localStorage.
-// That should be easy to port.
-// The issue here, is the fact that I don't want to store the data directly in AsyncStorage.
-const storage: StorageAdapter = {
-	read: async () => {
-		// Look for the persisted data in AsyncStorage.
-		return {} as SerializedEntries;
-	},
-	write: async batch => {
-		console.log(batch);
-		// Check if the data actually exists
-		// If it's value was undefined, remove it.
-		// If it does, then overwrite it.
-	}
-};
-
-const client = createClient({
-	url: env.apiUrl,
-	exchanges: [dedupExchange, cacheExchange({ storage }), fetchExchange]
-});
+const client = createClient({ url: env.apiUrl });
 
 const App = () => (
 	<UserProvider>
